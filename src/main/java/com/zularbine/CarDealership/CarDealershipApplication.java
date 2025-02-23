a56b41d2734cfbedfefbc552a19170de32f6cff0
kamalksu <zkamal@students.kennesaw.edu>
2025-02-22 21:47:27 -0500

    first commit

---
+++ src/main/java/com/zularbine/CarDealership/CarDealershipApplication.java
@@ -0,0 +1,13 @@
+package com.zularbine.CarDealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDealershipApplication.class, args);
+	}
+
+}
